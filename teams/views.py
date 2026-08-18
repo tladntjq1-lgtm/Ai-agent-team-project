0ecd0b9e9f7a9c66e1e91173a56ba97f302fc409
이상재 <tkdwodlaek@naver.com>
2026-08-18 12:20:50 +0900

    링크 되는것 까지 확인 완료

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+# 튜터가 팀을 편성하고 결과를 확인(통계 요약, 인원 수정)하는 화면. 
+# (가중치 기반의 팀 편성 알고리즘이 실행되고, 인원 추가/제외 시 DB 트랜잭션이 안전하게 처리되어야 하는, 이 프로젝트에서 가장 복잡한 로직이 담길 곳이야.)
+def admin_team_management_view(request):
+    return render(request, 'teams/admin_team_management.html')
+
+# 학생이 자신의 소속 팀명과 팀원들의 상세 프로필을 확인하는 화면. 
+# (요청한 학생의 user_id를 기준으로 속한 팀 테이블을 조인(Join)하여 팀원 목록을 가져오는 쿼리가 필요해.)
+def my_team_view(request):
+    return render(request, 'teams/my_team.html')
